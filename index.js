5c42f90479773738677dc1eeead6f96124abfc8f
Berint Moffett <berint.moffett@yotes.collegeofidaho.edu>
2022-01-08 17:49:59 +0000

    Starting to formulate a node.js server

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-const express = require('express')
+const express = require('express');
+const fs = require('fs')
+const app = express();
+const port = 5000;
+
+app.get('/', (req, res) => {
+    res.send('./index.html', {root: __dirname})
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
