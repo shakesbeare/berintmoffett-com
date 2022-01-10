424e628841ec5cf2602eea112a1d4464120bfd30
Berint Moffett <berint.moffett@yotes.collegeofidaho.edu>
2022-01-10 16:22:51 +0000

    Updated port handling and fixed favicon implementation for arcane page.

--- server.js
+++ server.js
@@ -3,7 +3,21 @@ const fs = require('fs');
 const path = require('path');
 const { nextTick } = require('process');
 const app = express();
-const port = 80;
+
+const PORT = process.env.PORT;
+const TEST_PORT = 5000;
+var USED_PORT;
+if (process.argv.length >= 3){
+    if (process.argv[2] == "test"){
+        USED_PORT = TEST_PORT;
+        console.log('Test port selected...')
+    }
+}
+else {
+    console.log("Production port selected")
+    USED_PORT = PORT;
+}
+
 
 app.use(function(req, res, next){
     console.log(`${req.ip} GET ${req.url}`);
@@ -38,6 +52,6 @@ app.get('/arcane-p2e', function (req, res) {
     res.sendFile('arcane-p2e.html', options);
 })
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log(`App listening at http://localhost:${port}`)
+app.listen(USED_PORT, () => {
+    console.log(`Server running on port ${USED_PORT}`)
 })
