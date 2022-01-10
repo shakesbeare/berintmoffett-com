0ab0cf40407b3aba85fe8dc718894ca915949b27
Berint Moffett <berint.moffett@yotes.collegeofidaho.edu>
2022-01-10 18:04:46 +0000

    fix commit

---
+++ src/NavBar.js
@@ -0,0 +1,16 @@
+import './css/NavBar.css';
+
+function NavBar() {
+  return (
+    <div className="NavBar">
+      <ul>
+        <li><a href="/">HOME</a></li>
+        <li><a href="arcane-p2e">TABLETOP GAMING</a></li>
+        <li><a href="compositions">COMPOSITIONS</a></li>
+        <li><a href="about-me">ABOUT ME</a></li>
+      </ul>
+    </div>
+  );
+}
+
+export default NavBar;
