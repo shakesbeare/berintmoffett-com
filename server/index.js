10b12e21e34e4411c42726aa560569dfd032c0f4
Berint Moffett <berint.moffett@yotes.collegeofidaho.edu>
2022-01-11 04:16:34 +0000

    Small typo, this should be the last one

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.use(express.static(path.resolve(__dirname, '../client/build')));x
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
