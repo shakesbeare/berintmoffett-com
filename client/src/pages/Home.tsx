3235ce4930ab50e5f558ce692fb4e9f3d11ab5f9
Berint Moffett <berint.moffett@yotes.collegeofidaho.edu>
2022-01-15 23:47:28 +0000

    Fixed the home page bug

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,19 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React from 'react';
 import "../css/layout.css"
 
 const Home: React.FC = () => {
-    useEffect(() => {
-        axios.get('/api/hello')
-        .then(res => setState(res.data))
-    }, [])
 
-    const [data, setState] = useState('');
     return (
         <div className="PageBody">
             <h1>Welcome to my website!</h1>
             <p>It's brand new, try the navbar!</p>
-            <p>{data}</p>
         </div>
     )
 };
