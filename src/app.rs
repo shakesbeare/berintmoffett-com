056956e67f591e9847868c3d7ec3e5f7ba1a0d63
Berint Moffett <berint.moffett@gmail.com>
2023-07-23 22:06:36 -0600

    Converted to Leptos/Actix

---
+++ src/app.rs
@@ -0,0 +1,101 @@
+use leptos::*;
+use leptos_meta::*;
+use leptos_router::*;
+
+use crate::about_me::AboutMe;
+
+#[component]
+pub fn App(cx: Scope) -> impl IntoView {
+    // Provides context that manages stylesheets, titles, meta tags, etc.
+    provide_meta_context(cx);
+
+    view! { cx,
+        // injects a stylesheet into the document <head>
+        // id=leptos means cargo-leptos will hot-reload this stylesheet
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous"/>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+
+        // sets the document title
+        <Title text="Welcome to Leptos"/>
+
+        // content for this welcome page
+        <Router>
+            <main>
+                <nav class="bg-dark navbar navbar-expand-lg text-light">
+                  <div class="container-fluid">
+                    <a class="navbar-brand text-reset" href="/">Home</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                      <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
+                        // navbar items
+                        <li class="nav-item">
+                          <a class="nav-link active text-reset" aria-current="page" href="/about-me">About Me</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link text-reset" href="/lessons">Lessons</a>
+                        </li>
+
+                        <li class="nav-item">
+                          <a class="nav-link text-reset" href="/programming">Programming</a>
+                        </li>
+
+                        <li class="nav-item">
+                          <a class="nav-link text-reset" href="/composition">Composition</a>
+                        </li>
+
+                        <li class="nav-item">
+                          <a class="nav-link text-reset" href="/account">Account</a>
+                        </li>
+
+                      </ul>
+                    </div>
+                  </div>
+                </nav>
+                <Routes>
+                    <Route path="" view=HomePage/>
+                    <Route path="/about-me" view=AboutMe/>
+                    <Route path="/*any" view=NotFound/>
+                </Routes>
+            </main>
+        </Router>
+    }
+}
+
+/// Renders the home page of your application.
+#[component]
+fn HomePage(cx: Scope) -> impl IntoView {
+    // Creates a reactive value to update the button
+    let (count, set_count) = create_signal(cx, 0);
+    let on_click = move |_| set_count.update(|count| *count += 1);
+
+    view! { cx,
+        <h1>"Welcome to Leptos"</h1>
+        <button on:click=on_click>"Click Me: " {count}</button>
+    }
+}
+
+/// 404 - Not Found
+#[component]
+fn NotFound(cx: Scope) -> impl IntoView {
+    // set an HTTP status code 404
+    // this is feature gated because it can only be done during
+    // initial server-side rendering
+    // if you navigate to the 404 page subsequently, the status
+    // code will not be set because there is not a new HTTP request
+    // to the server
+    #[cfg(feature = "ssr")]
+    {
+        // this can be done inline because it's synchronous
+        // if it were async, we'd use a server function
+        let resp = expect_context::<leptos_actix::ResponseOptions>(cx);
+        resp.set_status(actix_web::http::StatusCode::NOT_FOUND);
+    }
+
+    view! { cx,
+        <h1>"404: Page Not Found"</h1>
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,88 @@
+#[cfg(feature = "ssr")]
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    use std::io::Write;
+
+    use actix_files::Files;
+    use actix_web::*;
+    use leptos::*;
+    use leptos_actix::{generate_route_list, LeptosRoutes};
+    use leptos_start::app::*;
+
+    use log::{ LevelFilter, Level };
+    use env_logger::Builder;
+    use env_logger::fmt::Color;
+    use actix_web::middleware::Logger;
+
+    // env_logger::init_from_env(Env::default().default_filter_or("info"));
+    let mut builder = Builder::from_default_env();
+
+    builder
+        .format(|buf, record| {
+            let mut level_style = buf.style();
+
+            let color = match record.level() {
+                Level::Info => Color::Blue,
+                Level::Warn => Color::Yellow,
+                Level::Error => Color::Red,
+                _ => Color::Green,
+            };
+
+            level_style.set_color(color).set_bold(true);
+
+            writeln!(buf, "[{}] - {}", level_style.value(record.level()),  record.args())
+        })
+        .filter(None, LevelFilter::Info)
+        .init();
+
+    let conf = get_configuration(None).await.unwrap();
+    let addr = conf.leptos_options.site_addr;
+
+    // Generate the list of routes in your Leptos App
+    let routes = generate_route_list(|cx| view! { cx, <App/> });
+
+    HttpServer::new(move || {
+        let leptos_options = &conf.leptos_options;
+        let site_root = &leptos_options.site_root;
+
+        App::new()
+            .wrap(Logger::new("%t: %a - \u{001b}[35m%r \u{001b}[36m%s \u{001b}[37m- %Dms"))
+            .route("/api/{tail:.*}", leptos_actix::handle_server_fns())
+            // serve JS/WASM/CSS from `pkg`
+            .service(Files::new("/pkg", format!("{site_root}/pkg")))
+            // serve other assets from the `assets` directory
+            .service(Files::new("/assets", site_root))
+            // serve the favicon from /favicon.ico
+            .service(favicon)
+            .leptos_routes(
+                leptos_options.to_owned(),
+                routes.to_owned(),
+                |cx| view! { cx, <App/> },
+            )
+            .app_data(web::Data::new(leptos_options.to_owned()))
+        //.wrap(middleware::Compress::default())
+    })
+    .bind(&addr)?
+    .run()
+    .await
+}
+
+#[cfg(feature = "ssr")]
+#[actix_web::get("favicon.ico")]
+async fn favicon(
+    leptos_options: actix_web::web::Data<leptos::LeptosOptions>,
+) -> actix_web::Result<actix_files::NamedFile> {
+    let leptos_options = leptos_options.into_inner();
+    let site_root = &leptos_options.site_root;
+    Ok(actix_files::NamedFile::open(format!(
+        "{site_root}/favicon.ico"
+    ))?)
+}
+
+#[cfg(not(any(feature = "ssr", feature = "csr")))]
+pub fn main() {
+    // no client-side main function
+    // unless we want this to work with e.g., Trunk for pure client-side testing
+    // see lib.rs for hydration function instead
+    // see optional feature `csr` instead
+}
