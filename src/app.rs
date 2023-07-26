b05529982ca12b1f67d8090e0d6a873df93742ac
Berint Moffett <berint.moffett@gmail.com>
2023-07-26 14:55:39 -0600

    Update home page, created lessons page for real this time

--- src/app.rs
+++ src/app.rs
@@ -3,6 +3,7 @@ use leptos_meta::*;
 use leptos_router::*;
 
 use crate::about_me::AboutMe;
+use crate::lessons::Lessons;
 
 #[component]
 pub fn App(cx: Scope) -> impl IntoView {
@@ -58,6 +59,7 @@ pub fn App(cx: Scope) -> impl IntoView {
                 <Routes>
                     <Route path="" view=HomePage/>
                     <Route path="/about-me" view=AboutMe/>
+                    <Route path="/lessons" view=Lessons/>
                     <Route path="/*any" view=NotFound/>
                 </Routes>
             </main>
@@ -68,13 +70,12 @@ pub fn App(cx: Scope) -> impl IntoView {
 /// Renders the home page of your application.
 #[component]
 fn HomePage(cx: Scope) -> impl IntoView {
-    // Creates a reactive value to update the button
-    let (count, set_count) = create_signal(cx, 0);
-    let on_click = move |_| set_count.update(|count| *count += 1);
 
     view! { cx,
-        <h1>"Welcome to Leptos"</h1>
-        <button on:click=on_click>"Click Me: " {count}</button>
+        <div class="row text-center">
+            <h1>"Berint Moffett"</h1>
+            <h3>"Performer, Composer, Programmer"</h3>
+        </div>
     }
 }
 
--- src/lessons.rs
+++ src/lessons.rs
@@ -1,1 +1,66 @@
+use leptos::*;
+use markdown::to_html;
+use std::fs::read_to_string;
 
+#[server(GetLessonPolicy, "/api")]
+pub async fn get_lesson_policy() -> Result<String, ServerFnError> {
+    match read_to_string("assets/lessons/Lesson Policy 7-24-23 web.md") {
+        Ok(v) => return Ok(to_html(&v)),
+        Err(e) => {
+            return Err(ServerFnError::from(e));
+        }
+    }
+}
+
+#[server(GetLessonDetails, "/api")]
+pub async fn get_lesson_details() -> Result<String, ServerFnError> {
+    match read_to_string("assets/lessons/Lesson Details 7-26-23.md") {
+        Ok(v) => return Ok(to_html(&v)),
+        Err(e) => {
+            return Err(ServerFnError::from(e));
+        }
+    }
+}
+/// The Lessons Page
+#[component]
+pub fn Lessons(cx: Scope) -> impl IntoView {
+    let policy_resource = create_resource(cx, || (), |_| async move { get_lesson_policy().await });
+    let about_resource = create_resource(cx, || (), |_| async move { get_lesson_details().await });
+    view! { cx,
+        <div class="container">
+            <div class="row mb-3 p-2">
+                <h1 class="text-center">"Private Music Lessons"</h1>
+            </div>
+            <div class="row mb-3">
+                <div class="col-md-6">
+                    <Suspense fallback=move || view!{ cx, <p>"Loading..."</p> }.into_view(cx)>
+                        {move || match about_resource.read(cx) {
+                                Some(res) => {
+                                    let Ok(html) = res else {
+                                        return view!{ cx, <p>"An Error Occurred"</p>}.into_view(cx);
+                                    };
+                                    view!{ cx, <div inner_html=html /> }.into_view(cx)
+                                }
+                                None => view!{ cx, <p>"File Not Found"</p> }.into_view(cx)
+                            }
+                        }
+                    </Suspense>
+                </div>
+                <div class="col-md-6">
+                    <Suspense fallback=move || view!{ cx, <p>"Loading..."</p> }.into_view(cx)>
+                        {move || match policy_resource.read(cx) {
+                                Some(res) => {
+                                    let Ok(html) = res else {
+                                        return view!{ cx, <p>"An Error Occurred"</p>}.into_view(cx);
+                                    };
+                                    view!{ cx, <div inner_html=html /> }.into_view(cx)
+                                }
+                                None => view!{ cx, <p>"File Not Found"</p> }.into_view(cx)
+                            }
+                        }
+                    </Suspense>
+                </div>
+            </div>
+        </div>
+    }
+}
