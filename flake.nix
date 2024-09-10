0a516ad7ae7a750136dd797a0b6ca60cd50db325
Berint Moffett <berint.moffett@gmail.com>
2024-09-09 23:43:53 -0600

    nix flake for dev env

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+    description = "A very basic flake";
+
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    };
+    
+    outputs = { self, nixpkgs }: let
+        inherit (nixpkgs) lib;
+        forAllSystems = lib.genAttrs lib.systems.flakeExposed;
+    in {
+        devShells = forAllSystems (system:
+            let pkgs = nixpkgs.legacyPackages.${system}; in rec {
+                default = pkgs.mkShell {
+                    packages = with pkgs; [
+                        nodejs
+                    ];
+                    shellHook = "exec $SHELL";
+                };
+        });
+    };
+}
--- src/api.rs
+++ src/api.rs
@@ -1,13 +1,9 @@
-use std::collections::HashMap;
-use std::str::FromStr;
+use std::{collections::HashMap, str::FromStr};
 
-use axum::http::Response;
-use axum::Json;
-use axum::response::IntoResponse;
+use axum::{http::Response, response::IntoResponse, Json};
 use reqwest::Method;
 
 use crate::database::snake::LeaderboardEntry;
-
 
 #[derive(Debug, serde::Serialize, serde::Deserialize)]
 pub struct Highscores {
@@ -27,8 +23,12 @@ pub async fn get_snake_highscores() -> Response<String> {
         .unwrap()
 }
 
-pub async fn post_new_highscore(Json(entry): Json<LeaderboardEntry>) -> impl IntoResponse {
-    crate::database::snake::new_leaderboard_entry(entry).await.unwrap();
+pub async fn post_new_highscore(
+    Json(entry): Json<LeaderboardEntry>,
+) -> impl IntoResponse {
+    crate::database::snake::new_leaderboard_entry(entry)
+        .await
+        .unwrap();
 }
 
 pub async fn snake_leaderboard() -> Response<String> {
@@ -45,26 +45,34 @@ pub async fn snake_leaderboard() -> Response<String> {
 
 #[derive(serde::Deserialize)]
 struct ReleaseResponse {
-    assets: Vec<Asset>
+    assets: Vec<Asset>,
 }
 
 #[derive(serde::Deserialize)]
 struct Asset {
-    browser_download_url: String
+    browser_download_url: String,
 }
 
 pub async fn snake_update() -> impl IntoResponse {
     let client = reqwest::Client::new();
-    let url = reqwest::Url::from_str(  "https://api.github.com/repos/shakesbeare/rust-snake/releases/latest").unwrap();
+    let url = reqwest::Url::from_str(
+        "https://api.github.com/repos/shakesbeare/rust-snake/releases/latest",
+    )
+    .unwrap();
     let request = reqwest::Request::new(Method::GET, url);
-    let req = reqwest::RequestBuilder::from_parts(client, request).header("User-Agent", "berintmoffett-com-Updater").build().unwrap();
+    let req = reqwest::RequestBuilder::from_parts(client, request)
+        .header("User-Agent", "berintmoffett-com-Updater")
+        .build()
+        .unwrap();
     let client = reqwest::Client::new();
     let res = client.execute(req).await.unwrap();
-    let res: ReleaseResponse = serde_json::from_str(&res.text().await.unwrap()).unwrap();
-    let res = reqwest::get(&res.assets.first().unwrap().browser_download_url).await.unwrap();
+    let res: ReleaseResponse =
+        serde_json::from_str(&res.text().await.unwrap()).unwrap();
+    let res = reqwest::get(&res.assets.first().unwrap().browser_download_url)
+        .await
+        .unwrap();
     let bytes: Vec<u8> = res.bytes().await.unwrap().into();
     let tar = flate2::read::GzDecoder::new(&bytes as &[u8]);
     let mut archive = tar::Archive::new(tar);
     archive.unpack("./client/static/wasm/rust-snake").unwrap();
 }
-
